7a13e11a664326f89634d26529516466cd485901
sotetu79 <sotetu79@gmail.com>
2024-01-02 18:56:15 +0900

    search.dartの追加

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pokeca_wallet/views/Search.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -55,9 +56,7 @@ class _HomeScreenState extends State<HomeScreen> {
           });
         },
         children: [
-          Container(
-            color: Colors.red,
-          ),
+          const Search(),
           Container(
             color: Colors.blue,
           ),
