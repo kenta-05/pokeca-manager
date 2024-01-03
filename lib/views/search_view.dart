35abaf927da2ba30b59af464e7f1a37550f2bbe5
sotetu79 <sotetu79@gmail.com>
2024-01-03 14:11:27 +0900

    search_viewでの基本表示を作成

--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -12,50 +12,45 @@ class _SearchViewState extends State<SearchView> {
     'カード1の詳細情報',
     'カード2の詳細情報',
     'カード3の詳細情報',
-    'カード3の詳細情報',
-    'カード3の詳細情報',
-    'カード3の詳細情報',
-    'カード3の詳細情報',
-    // 他のカードデータを追加
+    'カード4の詳細情報',
+    'カード5の詳細情報',
+    'カード6の詳細情報',
   ];
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: (cardData.length / 3).ceil(), // カードデータの数を3で割り、切り上げることで行数を計算
-      itemBuilder: (BuildContext context, int row) {
+      itemCount: (cardData.length / 3).ceil(),
+      itemBuilder: (context, int index) {
         return Row(
           children: [
             // カード1
-            if (row * 3 < cardData.length)
+            if (index * 3 + 1 <= cardData.length)
               Expanded(
                 child: Card(
-                  elevation: 4.0,
                   child: ListTile(
-                    title: Text('カード ${row * 3 + 1}'),
-                    subtitle: Text(cardData[row * 3]),
+                    title: Text('カード ${index * 3 + 1}'),
+                    subtitle: Text(cardData[index * 3]),
                   ),
                 ),
               ),
             // カード2
-            if (row * 3 + 1 < cardData.length)
+            if (index * 3 + 2 <= cardData.length)
               Expanded(
                 child: Card(
-                  elevation: 4.0,
                   child: ListTile(
-                    title: Text('カード ${row * 3 + 2}'),
-                    subtitle: Text(cardData[row * 3 + 1]),
+                    title: Text('カード ${index * 3 + 2}'),
+                    subtitle: Text(cardData[index * 3 + 1]),
                   ),
                 ),
               ),
             // カード3
-            if (row * 3 + 2 < cardData.length)
+            if (index * 3 + 3 <= cardData.length)
               Expanded(
                 child: Card(
-                  elevation: 4.0,
                   child: ListTile(
-                    title: Text('カード ${row * 3 + 3}'),
-                    subtitle: Text(cardData[row * 3 + 2]),
+                    title: Text('カード ${index * 3 + 3}'),
+                    subtitle: Text(cardData[index * 3 + 2]),
                   ),
                 ),
               ),
