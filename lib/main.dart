9893b7a0d001a71dc0f35e05053d6c26adda9216
sotetu79 <sotetu79@gmail.com>
2024-01-15 09:03:03 +0900

    Firebase接続初期化

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,21 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pokeca_wallet/screens/home_screen.dart';
 import 'package:pokeca_wallet/themes/light.dart';
 import 'package:pokeca_wallet/themes/dark.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() => runApp(
-      const ProviderScope(
-        child: App(),
-      ),
-    );
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(
+    const ProviderScope(
+      child: App(),
+    ),
+  );
+}
 
 class App extends StatelessWidget {
   const App({super.key});
