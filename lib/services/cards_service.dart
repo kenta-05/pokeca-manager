030e3af30ceffc06318358ef46d86127e3951fbf
sotetu79 <sotetu79@gmail.com>
2024-01-21 07:04:55 +0900

    不要な型キャストを削除

--- lib/services/cards_service.dart
+++ lib/services/cards_service.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:pokeca_wallet/services/firebase_setup.dart';
 import 'package:pokeca_wallet/services/util.dart';
 
@@ -24,9 +23,7 @@ class CardsService {
 
       // extract actual data from DocumentSnapshot list
       yield* userDocRef.collection("cards").snapshots().map((snapshot) {
-        return snapshot.docs
-            .map((doc) => doc.data() as Map<String, dynamic>)
-            .toList();
+        return snapshot.docs.map((doc) => doc.data()).toList();
       });
     } catch (e) {
       print("Error getting document: $e");
