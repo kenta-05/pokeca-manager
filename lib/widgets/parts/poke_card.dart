ceeb4cdf09105bc850ffeaea3e00e24ad45b3c03
sotetu79 <sotetu79@gmail.com>
2024-01-21 19:28:45 +0900

    PokeCardの基本的表示完成

--- lib/widgets/parts/poke_card.dart
+++ lib/widgets/parts/poke_card.dart
@@ -15,6 +15,11 @@ class PokeCard extends StatelessWidget {
         height: 100,
         decoration: BoxDecoration(
           color: Theme.of(context).colorScheme.primaryContainer,
+          borderRadius: BorderRadius.circular(8),
+          border: Border.all(
+            color: Colors.black12,
+            width: 2,
+          ),
         ),
         child: Padding(
           padding: const EdgeInsets.all(8),
@@ -26,7 +31,9 @@ class PokeCard extends StatelessWidget {
                   style: Theme.of(context).textTheme.titleMedium),
               Text(cardData.packName,
                   style: Theme.of(context).textTheme.titleSmall),
-              Text(cardData.cost.toString()),
+              const Spacer(),
+              Text('${cardData.cost}円',
+                  style: Theme.of(context).textTheme.titleMedium),
             ],
           ),
         ),
