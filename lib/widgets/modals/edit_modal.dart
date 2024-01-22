3cc188d486f01dda56f28017e1e19f1aa6ec1f6a
sotetu79 <sotetu79@gmail.com>
2024-01-22 16:22:35 +0900

    基本的なEditModalの構造を作成

--- lib/widgets/modals/edit_modal.dart
+++ lib/widgets/modals/edit_modal.dart
@@ -1,10 +1,76 @@
 import 'package:flutter/material.dart';
 
-class EditModal extends StatelessWidget {
+class EditModal extends StatefulWidget {
   const EditModal({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
+  _EditModalState createState() => _EditModalState();
+}
+
+class _EditModalState extends State<EditModal> {
+  String titleValue = '';
+  String packNameValue = '';
+  String costValue = '';
+
+  @override
   Widget build(context) {
-    return AlertDialog();
+    return AlertDialog(
+      title: const Text('Edit Modal'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          TextFormField(
+            initialValue: titleValue,
+            decoration: const InputDecoration(labelText: 'Title'),
+            onChanged: (value) => titleValue = value,
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Please enter some text.';
+              } else if (value.length > 25) {
+                return 'Title must be less than 25 characters.';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            initialValue: packNameValue,
+            decoration: const InputDecoration(labelText: 'Pack Name'),
+            onChanged: (value) => packNameValue = value,
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Please enter some text.';
+              } else if (value.length > 25) {
+                return 'Pack Name must be less than 25 characters.';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            initialValue: costValue,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(labelText: 'Cost'),
+            onChanged: (value) => costValue = value,
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Please enter some text.';
+              } else if (double.tryParse(value) == null) {
+                return 'Cost must be an integer.';
+              }
+              return null;
+            },
+          ),
+        ],
+      ),
+      actions: <Widget>[
+        TextButton(
+          onPressed: () {
+            // Use titleValue, packNameValue, and costValue here
+            Navigator.of(context).pop();
+          },
+          child: const Text('Conferm'),
+        ),
+      ],
+    );
   }
 }
